416e11198daffaff5966ef71034a7bb30b251e1c
shanejackson <sjacksondev@gmail.com>
2017-03-01 10:58:15 -0800

    first commit

---
+++ src/SQLiteWeb/Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SQLiteWeb.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SQLiteWeb.Controllers
+{
+    public class StudentController : Controller
+    {
+        private SchoolContext _context;
+        public StudentController(SchoolContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Students.ToList());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Students.Add(student);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+    }
+}
---
+++ src/SQLiteWeb/Models/SchoolContext .cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SQLiteWeb.Models
+{
+    public class SchoolContext :DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        { }
+        public DbSet<Student> Students { get; set; }
+    }
+}
